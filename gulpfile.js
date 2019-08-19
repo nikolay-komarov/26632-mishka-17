602a26ce11e83f43514c1aab39a0b618eedfac57
kny45 <kny.komarov@gmail.com>
2019-08-20 00:36:30 +0300

    Сделал спрайт, добавил иконки

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -19,6 +21,15 @@ gulp.task("css", function () {
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("sprite", function () {
+  return gulp.src(["source/img/icon-*.svg", "source/img/logo-*.svg"])
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("server", function () {
